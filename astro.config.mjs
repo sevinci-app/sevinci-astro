ce13054d0ea1ef9ab4d9868ca45b107912f6c3db
Gabriel Zambrano <gab.zambrano@gmail.com>
2025-05-02 16:46:02 +0200

    astro config changed

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,13 +3,12 @@ import tailwind from "@astrojs/tailwind";
 import react from "@astrojs/react";
 import sitemap from "@astrojs/sitemap";
 import mdx from "@astrojs/mdx";
-
 import alpinejs from "@astrojs/alpinejs";
-
 import cloudflare from "@astrojs/cloudflare";
 
 // https://astro.build/config
 export default defineConfig({
   integrations: [tailwind(), react(), sitemap(), mdx(), alpinejs()],
+  output: 'server',
   adapter: cloudflare()
 });
